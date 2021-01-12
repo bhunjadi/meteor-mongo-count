a0408c44aed73d83814959ccb63347da5b0a9c5d
Berislav <berislav.hunjadi@gmail.com>
2021-01-12 09:40:24 +0100

    Meteor 1.12 and TypeScript 4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bhunjadi:mongo-count',
-    version: '0.0.1',
+    version: '1.0.0',
     // Brief, one-line summary of the package.
     summary: '',
     // URL to the Git repository containing the source code for this package.
@@ -11,14 +11,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.10.1');
+    api.versionsFrom('1.12');
     api.use('typescript');
     api.use('mongo');
     api.mainModule('index.ts', 'server');
 });
 
 Package.onTest(function(api) {
-    api.versionsFrom('1.10.1');
+    api.versionsFrom('1.12');
     api.use('meteortesting:mocha');
     api.use('typescript');
     api.use('mongo');
